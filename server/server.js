5f0156560e59d87f7d6bfb15743e747dbc4feeeb
JayHaymes <jjhaymes23@gmail.com>
2024-05-31 19:19:59 -0500

    updated api routes

--- server/server.js
+++ server/server.js
@@ -40,37 +40,61 @@ app.get("/api/v1/restaurants/:id", async (req, res) => {
     } catch (error) {
         console.log(error)
     }
-  
+
 });
 
 // Create a restaurant
-app.post("/api/v1/restaurants", (req, res) => {
-    console.log(req.body);
-    res.status(201).json({
-        status: "success",
-        data: {
-            restaurant: "mcdonalds"
-        }
-    });
+app.post("/api/v1/restaurants", async (req, res) => {
+    try {
+        const results = await db.query("INSERT INTO restaurants (name,location,price_range) VALUES ($1,$2,$3) RETURNING *", [
+            req.body.name,
+            req.body.location,
+            req.body.price_range
+        ]);
+        res.status(201).json({
+            status: "success",
+            data: {
+                restaurant: results.rows[0]
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
+
 });
 
 // Edit a restaurant
-app.put("/api/v1/restaurants/:id", (req, res) => {
-    console.log(req.params.id);
-    console.log(req.body);
-    res.status(200).json({
-        status: "success",
-        data: {
-            restaurant: "mcdonalds"
-        }
-    });
+app.put("/api/v1/restaurants/:id", async (req, res) => {
+    try {
+        const results = await db.query("UPDATE restaurants SET name= $1, location = $2, price_range = $3 WHERE id = $4 RETURNING *", [
+            req.body.name,
+            req.body.location,
+            req.body.price_range,
+            req.params.id
+        ]);
+        res.status(201).json({
+            status: "success",
+            data: {
+                restaurant: results.rows[0]
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
 });
 
 // Delete a restaurant
-app.delete("/api/v1/restaurants/:id", (req, res) => {
-    res.status(204).json({
-        status: "success"
-    });
+app.delete("/api/v1/restaurants/:id", async (req, res) => {
+    try {
+        const results = await db.query("DELETE FROM restaurants WHERE id = $1", [
+            req.params.id
+        ]);
+        res.status(204).json({
+            status: 'success'
+        });
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 
