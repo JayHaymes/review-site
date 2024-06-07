cd4c8c47aab8b844cae5ab67b85e7d666152a9ef
JayHaymes <jjhaymes23@gmail.com>
2024-06-06 19:15:18 -0500

    updated psql db & resturant detail page

--- DB/db.sql
+++ DB/db.sql
@@ -15,4 +15,12 @@ CREATE TABLE restaurants (
     price_range INT NOT NULL check(price_range >= 1 and price_range <= 5)
 );
 
-INSERT INTO restaurants (id,name,location,price_range) VALUES (123,'mcdonalds','new york',3);
+
+CREATE TABLE reviews (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
+    name VARCHAR(50) NOT NULL,
+    review TEXT NOT NULL,
+    rating INT NOT NULL check(rating >=1 and rating <=5)
+);
+
